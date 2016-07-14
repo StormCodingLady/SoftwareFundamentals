0e9d172c9db05a4c5fb6aa1517f0d0338261e51f
stormiarias <sanduagaarias@gmail.com>
2016-07-14 12:25:39 -0700

    Six scenarios settled.

--- QuestionFour/QuestionFour/Program.cs
+++ QuestionFour/QuestionFour/Program.cs
@@ -10,82 +10,147 @@ namespace QuestionFour
     {
         static void Main(string[] args)
         {
-            double a = 1.2;
-            double b = 1.4;
-            double c = 1.6;
+            double a = 0;
+            double b = 0;
+            double c = 0;
+            int intInput;
+            bool noError = false;
+            bool noError2 = false;
+            bool noError3 = false;
 
-            NumArrange(a, b, c);
-        }
+            while (noError == false)
+            {
+                Console.WriteLine("Integer 'a' equals...");
+                Console.WriteLine("(Enter an integer from the number pad)");
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out intInput);
+                if (isInt)
+                {
+                    a = intInput;
+                    noError = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input.");
+                    Console.WriteLine();
+                }
 
-        public static double[] AlphaSort(double a, double b, double c)
-        {
-            //do a lot of work.
-            return new double[] { a, b, c };
+            }
+
+            while (noError2 == false)
+            {
+                Console.WriteLine("Integer 'b' equals...");
+                Console.WriteLine("(Enter an integer from the number pad)");
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out intInput);
+                if (isInt)
+                {
+                    b = intInput;
+                    noError2 = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input.");
+                    Console.WriteLine();
+                }
+
+            }
+
+            while (noError3 == false)
+            {
+                Console.WriteLine("Integer 'c' equals...");
+                Console.WriteLine("(Enter an integer from the number pad)");
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out intInput);
+                if (isInt)
+                {
+                    c = intInput;
+                    noError3 = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid input.");
+                    Console.WriteLine();
+                }
+            }
+
+            NumSort(a, b, c);
+            Console.WriteLine("{0}, {1}, {2}");
+            Console.ReadLine();
         }
 
         public class Numbers
         {
-            public double a;
-            public double b;
-            public double c;
+            public double x;
+            public double y;
+            public double z;
         }
 
-        public static Numbers BravoSort(double a, double b, double c)
+        public static Numbers NumSort(double a, double b, double c)
         {
+            if (a < b && a < c)
+            {
+                if (b < c)
+                {
+
+                }
+                else
+                {
+                    double e;
+                    e = b;
+                    b = c;
+                    c = e;
+                }
+            }
+            else if (a > b && a < c)
+            {
+                double f;
+                f = a;
+                a = b;
+                b = f;
+            }
+            else if (a < b && a > c)
+            {
+                double g;
+                g = a;
+                a = c;
+                c = g;
+
+                if (b > c)
+                {
+                    double e;
+                    e = b;
+                    b = c;
+                    c = e;
+                }
+            }
+            else if (a > b && a > c)
+            {
+                double g;
+                g = a;
+                a = c;
+                c = g;
+
+                if (a < b)
+                {
+
+                }
+                else
+                {
+                    double f;
+                    f = a;
+                    a = b;
+                    b = f;
+                }
+            }
+
             Numbers retVal = new Numbers();
-            retVal.a = 5.0;
-            retVal.b = 7.0;
-            retVal.c = 10.0;
+            retVal.x = a;
+            retVal.y = b;
+            retVal.z = c;
             return retVal;
         }
 
-        public static double NumArrange(double a, double b, double c)
-        {
-            if (a <= b && a <= c) 
-            {
-                return a;
-
-                if (b <= c)
-                {
-                    return b;
-                    return c;
-                }
-                else
-                {
-                    return c;
-                    return b;
-                }
-            }
-            else if (b <= c && b <= a)
-            {
-                return b;
-
-                if (a <= c)
-                {
-                    return a;
-                    return c;
-                }
-                else
-                {
-                    return c;
-                    return a;
-                }
-            }
-            else if (c <= b && c <= a)
-            {
-                return c;
-
-                if (b <= a)
-                {
-                    return b;
-                    return a;
-                }
-                else
-                {
-                    return a;
-                    return b;
-                }
-            }
-        }
+        
     }
 }
