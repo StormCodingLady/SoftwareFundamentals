f7afbf7acc98d666bf67ed40353ec23cc334cccd
stormiarias <sanduagaarias@gmail.com>
2016-07-21 15:49:08 -0700

    User side initialized.

--- Chp6Lsn1/Chp6Lsn1/Program.cs
+++ Chp6Lsn1/Chp6Lsn1/Program.cs
@@ -6,10 +6,51 @@ using System.Threading.Tasks;
 
 namespace Chp6Lsn1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int n = 0;
+            int N = UsesrInput(n);
+            Console.ReadLine();
+            //PrintSequence(N);
+        }
+
+        public static int UsesrInput(int N)
+        {
+            int isInput;
+            bool resolved = false;
+            Console.WriteLine("Please enter an integer.");
+
+            while (resolved == false)
+            {
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out isInput);
+
+                if (isInt)
+                {
+                    N = isInput;
+                    resolved = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                    resolved = false;
+                }
+            }
+
+            return N;
+        }
+
+        public static void PrintSequence(int N)
+        {
+            int i = 1;
+
+            do
+            {
+                Console.Write(i);
+                i++;
+            } while (i <= N);
         }
     }
 }
