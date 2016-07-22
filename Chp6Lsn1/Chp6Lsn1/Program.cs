4018578006bc44f2a3e1d9bfaa743e189336eaf4
stormiarias <sanduagaarias@gmail.com>
2016-07-21 17:39:49 -0700

    Test Success.

--- Chp6Lsn1/Chp6Lsn1/Program.cs
+++ Chp6Lsn1/Chp6Lsn1/Program.cs
@@ -11,23 +11,23 @@ namespace Chp6Lsn1
         static void Main(string[] args)
         {
             int n = 0;
-            int N = UsesrInput(n);
-            Console.ReadLine();
-            //PrintSequence(N);
+            int m = UsesrInput(n);
+            int[] N = CalcSequence(m);
+            PrintSequence(N);
         }
 
         public static int UsesrInput(int N)
         {
             int isInput;
             bool resolved = false;
-            Console.WriteLine("Please enter an integer.");
+            Console.WriteLine("Please enter a positive integer.");
 
             while (resolved == false)
             {
                 string input = Console.ReadLine();
                 bool isInt = System.Int32.TryParse(input, out isInput);
 
-                if (isInt)
+                if (isInt && isInput > 0)
                 {
                     N = isInput;
                     resolved = true;
@@ -42,15 +42,24 @@ namespace Chp6Lsn1
             return N;
         }
 
-        public static void PrintSequence(int N)
+        public static void PrintSequence(int[] N)
+        {
+            Console.Write("", N);
+            Console.ReadLine();
+        }
+
+        public static int[] CalcSequence(int N)
         {
             int i = 1;
+            int[] holdSequence = new int[N];
 
             do
             {
-                Console.Write(i);
+                holdSequence[i-1] = i;
                 i++;
             } while (i <= N);
+
+            return holdSequence;
         }
     }
 }
--- Chp6Lsn1/Chp6Lsn1Test/Test.cs
+++ Chp6Lsn1/Chp6Lsn1Test/Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chp6Lsn1;
 
 namespace Chp6Lsn1Test
 {
@@ -9,6 +10,14 @@ namespace Chp6Lsn1Test
         [TestMethod]
         public void TestMethod1()
         {
+            int[] forFifteen = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            int[] N = Program.CalcSequence(15);
+
+            for (int i = 0; i < 15; i++)
+            {
+                Assert.IsTrue(N[i] == forFifteen[i]);
+            }
         }
     }
 }
