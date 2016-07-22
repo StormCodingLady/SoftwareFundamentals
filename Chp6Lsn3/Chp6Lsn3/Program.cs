50b971263fdcc9cb11c51e78addd9cd5632ef095
stormiarias <sanduagaarias@gmail.com>
2016-07-22 16:11:17 -0700

    Unit test and assignment finished.

--- Chp6Lsn3/Chp6Lsn3/Program.cs
+++ Chp6Lsn3/Chp6Lsn3/Program.cs
@@ -12,15 +12,22 @@ namespace Chp6Lsn3
         {
             int initialValue = 0;
             int[] N = UsesrInput(initialValue);
-            PrintSequence(N);
+            int[] endValues = SmallestAndLargest(N);
+            PrintSequence(endValues);
         }
 
+        /// <summary>
+        /// Takes in a series of values provided by the user.
+        /// </summary>
+        /// <param name="iV">Initial value for initial values sake.</param>
+        /// <returns>An array created by the user.</returns>
         public static int[] UsesrInput(int iV)
         {
             int isInput;
             bool resolved = false;
             int[] userSeries = new int[2];
             int i = 0;
+            int m = 1;
             Console.WriteLine("Please enter a series of positive integers. When finished enter the word 'done'.");
 
             while (resolved == false)
@@ -31,18 +38,16 @@ namespace Chp6Lsn3
 
                 if (isInt)
                 {
-                    userSeries[i] = isInput;
-                    i++;
-
-                    if (i > numColumns - 1)
+                    if (m > numColumns)
                     {
                         Array.Resize(ref userSeries, i + 1);
                     }
+                    userSeries[i] = isInput;
+                    i++;
+                    m++;
                 }
                 else if (input == "done")
                 {
-                    i++;
-                    Array.Resize(ref userSeries, i - 1);
                     resolved = true;
                 }
                 else
@@ -54,6 +59,33 @@ namespace Chp6Lsn3
             return userSeries;
         }
 
+        /// <summary>
+        /// Finds the smallest and largest integers in the given array.
+        /// </summary>
+        /// <param name="N">Provided Array</param>
+        /// <returns>Smallest and largest integers inside of a new array.</returns>
+        public static int[] SmallestAndLargest(int[] N)
+        {
+            int[] endValues = new int[2];
+            int minInt = N[0];
+            int maxInt = N[0];
+
+            foreach (int i in N)
+            {
+                if (i < minInt) minInt = i;
+                if (i > maxInt) maxInt = i;
+            }
+
+            endValues[0] = minInt;
+            endValues[1] = maxInt;
+
+            return endValues;
+        }
+
+        /// <summary>
+        /// Prints the given array to the console.
+        /// </summary>
+        /// <param name="N">Array containing the largest and smallest integers.</param>
         public static void PrintSequence(int[] N)
         {
             string foo = String.Join(", ", N);
--- Chp6Lsn3/UnitTestProject/UnitTest1.cs
+++ Chp6Lsn3/UnitTestProject/UnitTest1.cs
@@ -8,8 +8,31 @@ namespace UnitTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod()
+        public void TestMethod_ForPositives()
         {
+            int[] positiveInput = { 1, 2, 3, 4, 5 };
+            int[] positiveAnswer = { 1, 5 };
+
+            int numberColumns = positiveAnswer.GetLength(0);
+            for (int i = 0; i < numberColumns; i++)
+            {
+                int[] comparison = Program.SmallestAndLargest(positiveInput);
+                Assert.IsTrue(positiveAnswer[i] == comparison[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod_ForNegatives()
+        {
+            int[] negativeInput = { -5, -4, -3, -2, -1 };
+            int[] negativeAnswer = { -5, -1 };
+
+            int numberColumns = negativeAnswer.GetLength(0);
+            for (int i = 0; i < numberColumns; i++)
+            {
+                int[] comparison = Program.SmallestAndLargest(negativeInput);
+                Assert.IsTrue(negativeAnswer[i] == comparison[i]);
+            }
         }
     }
 }
