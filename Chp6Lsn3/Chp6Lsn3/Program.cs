6970cf2c67714f55e7af2e10bbc95d0fed0235ab
stormiarias <sanduagaarias@gmail.com>
2016-07-21 21:02:33 -0700

    User input added.

--- Chp6Lsn3/Chp6Lsn3/Program.cs
+++ Chp6Lsn3/Chp6Lsn3/Program.cs
@@ -10,6 +10,56 @@ namespace Chp6Lsn3
     {
         static void Main(string[] args)
         {
+            int initialValue = 0;
+            int[] N = UsesrInput(initialValue);
+            PrintSequence(N);
+        }
+
+        public static int[] UsesrInput(int iV)
+        {
+            int isInput;
+            bool resolved = false;
+            int[] userSeries = new int[2];
+            int i = 0;
+            Console.WriteLine("Please enter a series of positive integers. When finished enter the word 'done'.");
+
+            while (resolved == false)
+            {
+                int numColumns = userSeries.GetLength(0);
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out isInput);
+
+                if (isInt)
+                {
+                    userSeries[i] = isInput;
+                    i++;
+
+                    if (i > numColumns - 1)
+                    {
+                        Array.Resize(ref userSeries, i + 1);
+                    }
+                }
+                else if (input == "done")
+                {
+                    i++;
+                    Array.Resize(ref userSeries, i - 1);
+                    resolved = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+
+            return userSeries;
+        }
+
+        public static void PrintSequence(int[] N)
+        {
+            string foo = String.Join(", ", N);
+            Console.WriteLine(foo);
+
+            Console.ReadLine();
         }
     }
 }
