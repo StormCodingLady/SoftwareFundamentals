abce9b5eb7d22770df6e912e858d431789cd8334
stormiarias <sanduagaarias@gmail.com>
2016-07-23 13:06:15 -0700

    initial commit.

---
+++ Chp7Lsn1/UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chp7Lsn1;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_MultipleOfFive()
+        {
+        }
+    }
+}
