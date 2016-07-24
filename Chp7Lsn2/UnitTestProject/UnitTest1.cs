49715396599a476d41c69d2f0dc0c12badd60dcb
stormiarias <sanduagaarias@gmail.com>
2016-07-24 12:50:27 -0700

    Unit testing complete.

--- Chp7Lsn2/UnitTestProject/UnitTest1.cs
+++ Chp7Lsn2/UnitTestProject/UnitTest1.cs
@@ -8,9 +8,39 @@ namespace UnitTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod_ArrayEqual()
+        public void TestMethod_ArrayEqual_IsTrue()
         {
-            int[][] jaggedArray = new int[2][];
+            int[][] jaggedArray = {
+                new int[] { 1, 2, 3 },
+                new int[] { 1, 2, 3 }
+            };
+
+            bool isTrue = Program.EquivalentCheck(jaggedArray);
+            Assert.IsTrue(isTrue);
+        }
+
+        [TestMethod]
+        public void TestMethod_ArrayEqual_IsFalse_EqualIndex()
+        {
+            int[][] jaggedArray = {
+                new int[] { 1, 2, 3 },
+                new int[] { 1, 2, 6 }
+            };
+
+            bool isTrue = Program.EquivalentCheck(jaggedArray);
+            Assert.IsTrue(!isTrue);
+        }
+
+        [TestMethod]
+        public void TestMethod_ArrayEqual_IsFalse_UnequalIndex()
+        {
+            int[][] jaggedArray = {
+                new int[] { 1, 2, 3 },
+                new int[] { 1, 2, 3, 4 }
+            };
+
+            bool isTrue = Program.EquivalentCheck(jaggedArray);
+            Assert.IsTrue(!isTrue);
         }
     }
 }
