4fab9ae5248f7e983087bd17dbdf2fe24f20dcf1
stormiarias <sanduagaarias@gmail.com>
2016-07-24 12:11:09 -0700

    User Input managed.

--- Chp7Lsn2/Chp7Lsn2/Program.cs
+++ Chp7Lsn2/Chp7Lsn2/Program.cs
@@ -10,6 +10,69 @@ namespace Chp7Lsn2
     {
         static void Main(string[] args)
         {
+            int[][] jaggedArray = new int[2][];
+            int[][] toCompare = UsesrInput(jaggedArray);
+            ReadOut(toCompare);
+        }
+
+        public static int[][] UsesrInput(int[][] jaggedArray)
+        {
+            int isInput;
+            bool resolved = false;
+            jaggedArray[0] = new int[1];
+            jaggedArray[1] = new int[1];
+            int i = 0;
+            int index = 0;
+            Console.WriteLine("Please enter a series of integers for your first array. When finished enter");
+            Console.WriteLine("the word 'done'. Repeat for the second Array.");
+
+            while (resolved == false)
+            {
+                int numColumns = jaggedArray[index].GetLength(0);
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out isInput);
+
+                if (isInt)
+                {
+                    if (i + 1 > numColumns)
+                    {
+                        Array.Resize(ref jaggedArray[index], i + 1);
+                    }
+                    jaggedArray[index][i] = isInput;
+                    i++;
+                }
+                else if (input == "done" && index == 1)
+                {
+                    resolved = true;
+                }
+                else if (input == "done")
+                {
+                    i = 0;
+                    index++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+
+            return jaggedArray;
+        }
+
+        public static void ReadOut(int[][] toCompare)
+        {
+            int index = 0;
+
+            while (index < 2) {
+                for (int i = 0; i < toCompare[index].GetLength(0); i++)
+                {
+                    Console.Write("{0} ", toCompare[index][i]);
+                }
+                index++;
+                Console.WriteLine("");
+            }
+            
+            Console.ReadLine();
         }
     }
 }
