a4be073a9b99d89fcc2d7aa4e4709e22cf44765c
stormiarias <sanduagaarias@gmail.com>
2016-07-24 10:26:56 -0700

    Unit test finished, program finished.

--- Chp7Lsn1/Chp7Lsn1/Program.cs
+++ Chp7Lsn1/Chp7Lsn1/Program.cs
@@ -10,6 +10,31 @@ namespace Chp7Lsn1
     {
         static void Main(string[] args)
         {
+            int[] arrayEmpty = new int[20];
+
+            int [] displayArray = FillArray(arrayEmpty);
+            DisplayArray(displayArray);
+        }
+
+        public static int[] FillArray(int[] arrayEmpty)
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                arrayEmpty[i] = i * 5;
+            }
+
+            int[] arrayFull = arrayEmpty;
+            return arrayFull;
+        }
+
+        public static void DisplayArray(int[] displayArray)
+        {
+            for (int mulFive = 0; mulFive < 20; mulFive++)
+            {
+                Console.Write("{0} ", displayArray[mulFive]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- Chp7Lsn1/UnitTest/UnitTest1.cs
+++ Chp7Lsn1/UnitTest/UnitTest1.cs
@@ -10,6 +10,16 @@ namespace UnitTest
         [TestMethod]
         public void TestMethod_MultipleOfFive()
         {
+            int[] correctAns = { 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95 };
+            int[] arrayEmpty = new int[20];
+
+            int numColumns = correctAns.GetLength(0);
+
+            for (int i = 0; i < numColumns; i++)
+            {
+                int[] arrayFull = Program.FillArray(arrayEmpty);
+                Assert.IsTrue(correctAns[i] == arrayFull[i]);
+            }
         }
     }
 }
