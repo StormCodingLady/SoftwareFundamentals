275ad935ade8d8bcce6117a5489d9fd98cc9cffb
stormiarias <sanduagaarias@gmail.com>
2016-07-26 16:41:49 -0400

    User input handled.

--- Chp8Lsn4/Chp8Lsn4/Program.cs
+++ Chp8Lsn4/Chp8Lsn4/Program.cs
@@ -10,6 +10,32 @@ namespace Chp8Lsn4
     {
         static void Main(string[] args)
         {
+            Console.Write(UserInput());
+            Console.ReadLine();
+        }
+
+        public static int UserInput()
+        {
+            int isInput = 0;
+            bool resolved = false;
+            Console.WriteLine("Please enter an integer. The integer will be converted to binary form.");
+
+            while (resolved == false)
+            {
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out isInput);
+
+                if (isInt)
+                {
+                    resolved = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+
+            return isInput;
         }
     }
 }
