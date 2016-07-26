3192cc1eb655fa84e1459d02d127ca57a01c4f65
stormiarias <sanduagaarias@gmail.com>
2016-07-26 17:30:40 -0400

    Feedback complete.

--- Chp8Lsn4/Chp8Lsn4/Program.cs
+++ Chp8Lsn4/Chp8Lsn4/Program.cs
@@ -10,8 +10,9 @@ namespace Chp8Lsn4
     {
         static void Main(string[] args)
         {
-            Console.Write(UserInput());
-            Console.ReadLine();
+            int decimalNumber = UserInput();
+            double[] binaryNumber = DecimalToBinary(decimalNumber);
+            DisplayBinaryNumber(binaryNumber, decimalNumber);
         }
 
         public static int UserInput()
@@ -36,6 +37,45 @@ namespace Chp8Lsn4
             }
 
             return isInput;
+        }
+
+        public static double[] DecimalToBinary(double numberDecimal)
+        {
+            double[] binaryHolder = new double[1];
+            int i = 0;
+
+            while(numberDecimal >= 1)
+            {
+                int numColumns = binaryHolder.GetLength(0);
+                
+                if(i + 1 > numColumns)
+                {
+                    Array.Resize(ref binaryHolder, i + 1);
+                }
+
+                double remainder = numberDecimal % 2; // 1 or 0
+                binaryHolder[i] = remainder;
+                numberDecimal = Math.Floor(numberDecimal / 2);
+                i++;
+            }
+
+            Array.Reverse(binaryHolder);
+            return binaryHolder;
+        }
+
+        public static void DisplayBinaryNumber(double[] binaryNumber, int signIndicator)
+        {
+            if(signIndicator < 0)
+            {
+                Console.WriteLine("");
+                Console.Write("-");
+                String.Join("", binaryNumber);
+            }
+            else
+            {
+                Console.WriteLine("");
+                String.Join("", binaryNumber);
+            }
         }
     }
 }
