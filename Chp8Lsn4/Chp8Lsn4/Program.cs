63f98621fd9a4f2746fef03ff4189dc9232702e7
stormiarias <sanduagaarias@gmail.com>
2016-07-26 15:41:30 -0400

    initial commit.

---
+++ Chp8Lsn4/Chp8Lsn4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chp8Lsn4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
