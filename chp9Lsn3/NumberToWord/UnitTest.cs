30b7462157dc3bad6009c8742303655afb77b471
stormiarias <sanduagaarias@gmail.com>
2016-07-28 15:31:16 -0400

    Success of red-green board.

--- chp9Lsn3/NumberToWord/UnitTest.cs
+++ chp9Lsn3/NumberToWord/UnitTest.cs
@@ -8,8 +8,19 @@ namespace NumberToWord
     public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_CaptureLastDigit()
         {
+            int testValue = NumberConversion.CaptureLastDigit(123);
+
+            Assert.IsTrue(testValue == 3);
+        }
+
+        [TestMethod]
+        public void TestMethod_NameOfDigit()
+        {
+            string testWord = NumberConversion.NameOfDigit(7);
+
+            Assert.IsTrue(testWord == "Seven");
         }
     }
 }
