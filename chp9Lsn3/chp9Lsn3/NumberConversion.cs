4159d81d6809d458535406e718a260e5d0cfec63
stormiarias <sanduagaarias@gmail.com>
2016-07-28 15:23:09 -0400

    Program written.

--- chp9Lsn3/chp9Lsn3/NumberConversion.cs
+++ chp9Lsn3/chp9Lsn3/NumberConversion.cs
@@ -10,6 +10,95 @@ namespace Chp9Lsn3
     {
         static void Main(string[] args)
         {
+            int value = UserInput();
+            value = CaptureLastDigit(value);
+            string englishName = NameOfDigit(value);
+            PrintWord(englishName);
+
+        }
+
+        public static int UserInput()
+        {
+            int isInput = 0;
+            bool resolved = false;
+            Console.WriteLine("Please enter an integer.");
+
+            while (resolved == false)
+            {
+                string input = Console.ReadLine();
+                bool isInt = System.Int32.TryParse(input, out isInput);
+
+                if (isInt && isInput > 0)
+                {
+                    resolved = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                    resolved = false;
+                }
+            }
+
+            return isInput;
+        }
+
+        public static int CaptureLastDigit(int number)
+        {
+            number %= 10;
+            return number; 
+        }
+
+        public static string NameOfDigit(int digit)
+        {
+            string digitName = "Error";
+
+            switch (digit)
+            {
+                case 0:
+                    digitName = "Zero";
+                    break;
+                case 1:
+                    digitName = "One";
+                    break;
+                case 2:
+                    digitName = "Two";
+                    break;
+                case 3:
+                    digitName = "Three";
+                    break;
+                case 4:
+                    digitName = "Four";
+                    break;
+                case 5:
+                    digitName = "Five";
+                    break;
+                case 6:
+                    digitName = "Six";
+                    break;
+                case 7:
+                    digitName = "Seven";
+                    break;
+                case 8:
+                    digitName = "Eight";
+                    break;
+                case 9:
+                    digitName = "Nine";
+                    break;
+                case 10:
+                    digitName = "Ten";
+                    break;
+                default:
+                    digitName = "Error";
+                    break;
+            }
+
+            return digitName;
+        }
+
+        public static void PrintWord(string numberWord)
+        {
+            Console.Write("The English name of the last digit is '{0}'", numberWord);
+            Console.Read();
         }
     }
 }
