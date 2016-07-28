a138698acfa39e8355714dd48df58bdb53d7a972
stormiarias <sanduagaarias@gmail.com>
2016-07-28 18:06:34 -0400

    unit test complete

---
+++ Chp9Lsn5/UnitTestProject/UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chp9Lsn5;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod_IsItLarger_SansOutsideCase()
+        {
+            int[] randomArray = { 2, 5, 2, 3, 4, 2, 8, 9, 7, 9 };
+            bool[] answer = { true, false, false, true, false, false, true, false };
+
+            for (int i = 1; i < 9; i++)
+            {
+                bool higherValue = Program.IsLargerThan(randomArray, i);
+                Assert.IsTrue(higherValue == answer[i - 1]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod_IsItLarger_OutsideCase()
+        {
+            int[] randomArray = { 2, 5, 2, 3, 4, 2, 8, 9, 7, 9 };
+            bool[] answer = { false, true };
+
+            for (int i = 0; i < 10; i = i + 9)
+            {
+                int index = i;
+                if (i == 9)
+                {
+                    index = 1;
+                }
+
+                bool higherValue = Program.OutsideCase(randomArray, i);
+                Assert.IsTrue(higherValue == answer[index]);
+            }
+        }
+    }
+}
