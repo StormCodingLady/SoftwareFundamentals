48941b5f658b50d7a1aba3e0c4ea968bb7b98a48
stormiarias <sanduagaarias@gmail.com>
2016-09-13 15:51:09 -0700

    User input added.

--- Chp12Lsn7/Chp12Lsn7/Program.cs
+++ Chp12Lsn7/Chp12Lsn7/Program.cs
@@ -10,7 +10,63 @@ namespace Chp12Lsn7
     {
         static void Main(string[] args)
         {
-            //intial commit.
+            UserInput();
+            Console.ReadLine();
+        }
+
+        public static int UserInput()
+        {
+            Console.WriteLine("Please enter a positive integer greater than zero.");
+            Console.WriteLine("");
+            Console.Write("> ");
+            string userInput = Console.ReadLine();
+            int userNumber = 0;
+
+            try
+            {
+                userNumber = Convert.ToInt32(userInput);
+                
+                if(userNumber <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                if (userNumber > Int32.MaxValue || userNumber < Int32.MinValue)
+                {
+                    throw new OverflowException();
+                }
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Invalid Input! Cannot enter a number less than or equal to zero.");
+                throw;
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Invalid Input! Cannot enter non-numbers or non-integers.");
+                throw;
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Invalid Input! Cannot enter integers larger than or less then computers max/min value.");
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Thank You! Press any key to continue.");
+                Console.Read();
+            }
+
+            return userNumber;
+        }
+
+        public static void CalculateSquareRoot()
+        {
+
         }
     }
 }
