9888c2343af2d898b16cff846a6ffa8db42d875e
stormiarias <sanduagaarias@gmail.com>
2017-06-06 18:15:59 -0700

    failure.

--- Chp13Lsn2/Chp13Lsn2/Program.cs
+++ Chp13Lsn2/Chp13Lsn2/Program.cs
@@ -10,6 +10,31 @@ namespace Chp13Lsn2
     {
         static void Main(string[] args)
         {
+            string backwardsIntro = Reverse(Hello.x);
+            Print(backwardsIntro);
+        }
+
+        public class Hello
+        {
+            public static string x = "introduction";
+        }
+
+        public static string Reverse(string x)
+        {
+            string xOutput = "";
+
+            for (int i = x.Length - 1; i >= 0; i--)
+            {
+                xOutput += x[i];
+            }
+
+            return xOutput;
+        }
+
+        public static void Print(string intro)
+        {
+            Console.WriteLine(intro);
+            Console.ReadLine();
         }
     }
 }
