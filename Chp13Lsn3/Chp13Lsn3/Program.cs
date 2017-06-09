2177b6bdfec4fa337cdf750fc153831cad839033
stormiarias <sanduagaarias@gmail.com>
2017-06-09 16:43:05 -0700

    TooEasy

---
+++ Chp13Lsn3/Chp13Lsn3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chp13Lsn3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Today.DayOfWeek);
+            Console.ReadLine();
+        }
+    }
+}
