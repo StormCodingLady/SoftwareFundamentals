a33d7a415c73cf782090b3fe2284098524272818
stormiarias <sanduagaarias@gmail.com>
2017-07-17 10:16:22 -0700

    Inside code completed.

--- Chp10Lsn2/Chp10Lsn2/Print.cs
+++ Chp10Lsn2/Chp10Lsn2/Print.cs
@@ -6,8 +6,50 @@ using System.Threading.Tasks;
 
 namespace Chp10Lsn2
 {
-    class Print
+    public class Kofm
     {
+        private int k;
+        private int m;
+        
+        public Kofm (int k = 2, int m = 3)
+        {
+            this.k = k;
+            this.m = m;
+        }
 
+        /// <summary>
+        /// This seeds the recursion.
+        /// </summary>
+        public void Solve()
+        {
+            var tuples = Enumerable
+                .Repeat(m, k)
+                .ToArray();
+
+            int placeValue = k - 1;
+
+            VariableVariations(tuples, placeValue);
+        }
+
+        /// <summary>
+        /// This performs the actual recursion.
+        /// </summary>
+        /// <param name="tuples"></param>
+        /// <returns></returns>
+        private void VariableVariations(int[] tuples, int placeValue)
+        {
+            if (m > 1)
+            {
+                m -= 1;
+                tuples[placeValue] = m;
+                VariableVariations(tuples, placeValue);
+            }
+            else if (m == 1 && placeValue > 0)
+            {
+                placeValue -= 1;
+                tuples[placeValue] = m;
+                VariableVariations(tuples, placeValue);
+            }
+        }
     }
 }
